d386b31482296b993a992a8c4337a6d943269ec5
Ramneek Singh <ramneek.singh@csiro.cu>
2016-11-25 05:25:53 +1100

    init

---
+++ numericomp/numericomp/numericomp.cpp
@@ -0,0 +1,22 @@
+// numericomp.cpp : Defines the exported functions for the DLL application.
+//
+
+#include "stdafx.h"
+#include "numericomp.h"
+
+
+// This is an example of an exported variable
+NUMERICOMP_API int nnumericomp=0;
+
+// This is an example of an exported function.
+NUMERICOMP_API int fnnumericomp(void)
+{
+    return 42;
+}
+
+// This is the constructor of a class that has been exported.
+// see numericomp.h for the class definition
+Cnumericomp::Cnumericomp()
+{
+    return;
+}
---
+++ numericomp/numericomp/numericomp.h
@@ -0,0 +1,22 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the NUMERICOMP_EXPORTS
+// symbol defined on the command line. This symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// NUMERICOMP_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef NUMERICOMP_EXPORTS
+#define NUMERICOMP_API __declspec(dllexport)
+#else
+#define NUMERICOMP_API __declspec(dllimport)
+#endif
+
+// This class is exported from the numericomp.dll
+class NUMERICOMP_API Cnumericomp {
+public:
+	Cnumericomp(void);
+	// TODO: add your methods here.
+};
+
+extern NUMERICOMP_API int nnumericomp;
+
+NUMERICOMP_API int fnnumericomp(void);
